59583275d13b64ae9828a67aebf81c1d7664f4a7
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-04-29 13:18:04 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+
+$cars = array("Toyota","Mitsubishi","Nissan","Honda");
+
+echo"<ol>";
+foreach ($cars as $cars) 
+{
+    echo "<li> car </li>";
+
+}
+echo "</ol>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <class = "list-container">
+        <?php
+
+
+        ?>
+    </div>
+</body>
+</html>
